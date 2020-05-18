5d6a05ddf47a50e28af47bd26f9f07c8f47f1b83
kgorecki <krzysztof.gorecki@nokia.com>
2020-05-18 15:37:40 +0200

    new commit

---
+++ src/components/SummaryDashboardTable.jsx
@@ -0,0 +1,190 @@
+import React from "react";
+import {Paper, Table} from "@material-ui/core";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+// import update from 'immutability-helper';
+
+// const type = 'DragableBodyRow';
+//
+// const DragableBodyRow = ({ index, moveRow, className, style, ...restProps }) => {
+//     const ref = React.useRef();
+//     const [{ isOver, dropClassName }, drop] = useDrop({
+//         accept: type,
+//         collect: monitor => {
+//             const { index: dragIndex } = monitor.getItem() || {};
+//             if (dragIndex === index) {
+//                 return {};
+//             }
+//             return {
+//                 isOver: monitor.isOver(),
+//                 dropClassName: dragIndex < index ? ' drop-over-downward' : ' drop-over-upward',
+//             };
+//         },
+//         drop: item => {
+//             moveRow(item.index, index);
+//         },
+//     });
+//     const [, drag] = useDrag({
+//         item: { type, index },
+//         collect: monitor => ({
+//             isDragging: monitor.isDragging(),
+//         }),
+//     });
+//     drop(drag(ref));
+//
+//     return (
+//         <tr
+//             ref={ref}
+//             className={`${className}${isOver ? dropClassName : ''}`}
+//             style={{ cursor: 'move', ...style }}
+//             {...restProps}
+//         />
+//     );
+// };
+
+const SummaryDashboardTable = () => {
+
+    const createRow = () => {
+        return (
+            <TableRow>
+                <TableCell>
+                    first
+                </TableCell>
+                <TableCell>
+                    second
+                </TableCell>
+                <TableCell>
+                    third
+                </TableCell>
+            </TableRow>
+        );
+    };
+
+    return (
+        <Paper style={{height: "40%", width: "40%"}}>
+            <Table>firstHead
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            firstHead
+                        </TableCell>
+                        <TableCell>
+                            secondHead
+                        </TableCell>
+                        <TableCell>
+                            thirdHead
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {/*<DragableBodyRow index={0}>*/}
+                        {createRow()}
+                        {createRow()}
+                        {createRow()}
+                    {/*</DragableBodyRow>*/}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+};
+
+export default SummaryDashboardTable;
+
+// const columns = [
+//     {
+//         title: 'Name',
+//         dataIndex: 'name',
+//         key: 'name',
+//     },
+//     {
+//         title: 'Age',
+//         dataIndex: 'age',
+//         key: 'age',
+//     },
+//     {
+//         title: 'Address',
+//         dataIndex: 'address',
+//         key: 'address',
+//     },
+// ];
+
+// class DragSortingTable extends React.Component {
+//     state = {
+//         data: [
+//             {
+//                 key: '1',
+//                 name: 'John Brown',
+//                 age: 32,
+//                 address: 'New York No. 1 Lake Park',
+//             },
+//             {
+//                 key: '2',
+//                 name: 'Jim Green',
+//                 age: 42,
+//                 address: 'London No. 1 Lake Park',
+//             },
+//             {
+//                 key: '3',
+//                 name: 'Joe Black',
+//                 age: 32,
+//                 address: 'Sidney No. 1 Lake Park',
+//             },
+//         ],
+//     };
+//
+//     components = {
+//         body: {
+//             row: DragableBodyRow,
+//         },
+//     };
+//
+//     moveRow = (dragIndex, hoverIndex) => {
+//         const { data } = this.state;
+//         const dragRow = data[dragIndex];
+//
+//         this.setState(
+//             update(this.state, {
+//                 data: {
+//                     $splice: [
+//                         [dragIndex, 1],
+//                         [hoverIndex, 0, dragRow],
+//                     ],
+//                 },
+//             }),
+//         );
+//     };
+    //
+    // render() {
+    //     return (
+    //         <DndProvider backend={HTML5Backend}>
+    //             <Paper style={{height: "40%", width: "40%"}}>
+    //                 <Table>firstHead
+    //                     <TableHead>
+    //                         <TableRow>
+    //                             <TableCell>
+    //                                 firstHead
+    //                             </TableCell>
+    //                             <TableCell>
+    //                                 secondHead
+    //                             </TableCell>
+    //                             <TableCell>
+    //                                 thirdHead
+    //                             </TableCell>
+    //                         </TableRow>
+    //                     </TableHead>
+    //                     <TableBody>
+    //                         {createRow()}
+    //                         {createRow()}
+    //                         {createRow()}
+    //                     </TableBody>
+    //                 </Table>
+    //             </Paper>
+    //         </DndProvider>
+    //     );
+    // }
+// }
